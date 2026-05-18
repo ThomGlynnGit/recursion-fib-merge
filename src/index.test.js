44ce23c4a9f3be2a53261068f970f3ab19e5da7d
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-05-18 15:02:54 +0100

    Create simple fibonacci tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,11 +2,17 @@ import { fibonacci, mergeSort } from "./index.js";
 
 describe("fibonacci", () => {
   test("returns 0 if 0 provided", () => {
-    expect(fibonacci(0)).toBe(0);
+    expect(fibonacci(0)).toBe([0]);
+  });
+  test("returns correct sequence for n = 2", () => {
+    expect(fibonacci(3)).toBe([0, 1, 1]);
+  });
+  test("returns correct sequence for n = 8", () => {
+    expect(fibonacci(3)).toBe([0, 1, 1, 2, 3, 5, 8, 13]);
   });
 });
 
-describe("mergeSort", () => {
+describe.skip("mergeSort", () => {
   test("returns empty list if empty list provided", () => {
     expect(mergeSort([])).toStrictEqual([]);
   });
